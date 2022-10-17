cd3f72bd69f0c21326890c03b4644135702eab62
MaksimTeg <77045401+MaksimTeg@users.noreply.github.com>
2022-10-17 17:21:40 +0300

    fix: fix some bugs

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(matrix) {
+//    throw new NotImplementedError('Not implemented');
+  // let arrCombined = matrix.reduce(function(result, current){
+  //   return result(current)
+  // },[]);
+  let cats = [];
+  let columns = matrix.length;
+  for (let i = 0; i < columns; i++) {
+    cats = cats.concat(matrix[i].filter(cat => (cat == '^^')))
+    
+  }
+  //let cats = arrCombined.filter(cat => (cat == '^^'))
+  return cats.length
 }
 
+
 module.exports = {
-  countCats
+  countCats,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+// let Data = new Date()
+function getSeason(date) {
+// throw new NotImplementedError('Not implemented');
+if (!date) return 'Unable to determine the time of year!'
+let result = ""
 
+if (date instanceof Date) {
+if (date.hasOwnProperty('toString')) throw new Error('Invalid date!')
+const numMonth = date.getMonth()
+if (numMonth == 11 || numMonth == 0 || numMonth == 1){
+  result = "winter"
+} else if (numMonth == 2 || numMonth == 3 || numMonth == 4){
+  result = "spring"
+} else if (numMonth == 5 || numMonth == 6 || numMonth == 7){
+  result = "summer"
+} else if (numMonth == 8 || numMonth == 9 || numMonth == 10){
+  result = "autumn"
+} else { result= ("Error time of year")}
+return result;// 
+}else {
+  throw new Error('Invalid date!')
+}
+}
 module.exports = {
   getSeason
 };
